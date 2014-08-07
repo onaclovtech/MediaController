46b6cfe9df49f9d4084697175f8ecc076a8d7d8c
Tyson Bailey <onaclov2000+github@gmail.com>
2014-08-07 08:17:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,63 @@
 /*
 * MediaController v0.0.1
 */
+
+// Now if you look at FB_URL you'll see a new entry with Local and IP address
+// Next we'll do an SSDP search and find any "interesting" devices (particularly your chromecast or roku devices).
+// Needs cleanup but this reports those devices
+
 var connect = require('connect'),
-http = require('http');
+    http = require('http');
 connect()
-.use(connect.static('path you wish to serve'))
-.use(connect.directory('path you wish to serve'))
-.listen(80); // I use 80 on my local network, you could use 8080
+    .use(connect.static('path you wish to serve'))
+    .use(connect.directory('path you wish to serve'))
+    .listen(80); // I use 80 on my local network, you could use 8080
 
 
 var FB_URL = "";
 var Firebase = require('firebase');
 var myRootRef = new Firebase(FB_URL);
-var ip = ""; // I'll set this up to find it by itself in the near future.
-myRootRef.push({"type" : "local", "ip" : ip});
+var ip = '';
+myRootRef.push({
+    "type": "local",
+    "ip": ip
+});
 
-// Now if you look at FB_URL you'll see a new entry with Local and IP address
-// Next we'll do an SSDP search and find any "interesting" devices (particularly your chromecast or roku devices.
-// Coming soon
+
+var dgram = require('dgram'); // dgram is UDP
+// Listen for responses
+function listen(port) {
+    var server = dgram.createSocket("udp4");
+    server.on("message", function(msg, rinfo) {
+        //console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+        myRootRef.push({
+            "type": "roku",
+            "ip": rinfo.address,
+            "port": rinfo.port
+        });
+    });
+    server.bind(port); // Bind to the random port we were given when sending the message, not 1900
+    // Give it a while for responses to come in
+    setTimeout(function() {
+        //console.log("Finished waiting");
+        server.close();
+    }, 20000);
+}
+
+function search() {
+    var message = new Buffer(
+        "M-SEARCH * HTTP/1.1\r\n" +
+        "Host:239.255.255.250:1900\r\n" +
+        "Man:\"ssdp:discover\"\r\n" +
+        "ST: roku:ecp\r\n" + // Essential, used by the client to specify what they want to discover, eg 'ST:ge:fridge'
+        "MX:2\r\n" + // 1 second to respond (but they all respond immediately?)
+        "\r\n"
+    );
+    var client = dgram.createSocket("udp4");
+    client.bind(1900, "239.255.255.250"); // So that we get a port so we can listen before sending
+    listen(client.address().port);
+    client.send(message, 0, message.length, 1900, "239.255.255.250", function(err, bytes) {
+        client.close();
+    });
+}
+search();
