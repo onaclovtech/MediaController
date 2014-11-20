54c50b7704eaf7c29ddd9c55e6f7932ee8a38906
Tyson Bailey <onaclov2000+github@gmail.com>
2014-11-19 22:44:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,15 @@
 //https://www.firebase.com/docs/web/quickstart.html
 
 
-// File Server for the movies
-var connect = require('connect'),
-    http = require('http');
-var path_to_share = ''
-connect()
-    .use(connect.static(path_to_share))
-    .use(connect.directory(path_to_share))
-    .listen(80);
-console.log("created server");
-devices = [];
+// This also needs a little work, we don't have a server included in this file, we have to kick off our own running server.
+// I mainly wanted this out here in the event I have a system failure, I have it backed up somewhere somehow.
+chromecastjs = require('chromecast-js')
 var FB_URL = '';
 var Firebase = require('firebase');
 var myRootRef = new Firebase(FB_URL);
 var os = require('os')
-
+devices = [];
+var browser = new chromecastjs.Browser()
 var interfaces = os.networkInterfaces();
 var addresses = [];
 for (k in interfaces) {
@@ -33,128 +27,30 @@ for (k in interfaces) {
     }
 }
 
-// Pushing local device out there (so if you're in network you can play movies)
 devices.push(myRootRef.push({
     "type": "local",
     "ip": addresses[0]
 }));
 
-// Roku Discovery Section
-var roku_index
-var dgram = require('dgram'); // dgram is UDP
-// Listen for responses
-function listen(port) {
-    var server = dgram.createSocket("udp4");
-    server.on("message", function(msg, rinfo) {
-        devices.push(myRootRef.push({
-            "type": "roku",
-            "ip": rinfo.address,
-            "port": rinfo.port,
+browser.on('deviceOn', function(device){
+  device.connect()
+  device.on('connected', function(){
+devices.push(myRootRef.push({
+            "type": "chromecast",
             "state": "waiting"
         }));
-        console.log("Got the Roku's IP");
-    });
-    server.bind(port); // Bind to the random port we were given when sending the message, not 1900
-    // Give it a while for responses to come in
-    setTimeout(function() {
-        server.close();
-    }, 20000);
-}
-
-function search() {
-    var message = new Buffer(
-        "M-SEARCH * HTTP/1.1\r\n" +
-        "Host:239.255.255.250:1900\r\n" +
-        "Man:\"ssdp:discover\"\r\n" +
-        "ST: roku:ecp\r\n" + // Essential, used by the client to specify what they want to discover, eg 'ST:ge:fridge'
-        "MX:2\r\n" + // 1 second to respond (but they all respond immediately?)
-        "\r\n"
-    );
-    var client = dgram.createSocket("udp4");
-    client.bind(1900, "239.255.255.250"); // So that we get a port so we can listen before sending
-    listen(client.address().port);
-    client.send(message, 0, message.length, 1900, "239.255.255.250", function(err, bytes) {
-        client.close();
-    });
-}
-search();
-
-
-
-
-/* 
- * roku control
- */
-function send_message(options) {
-    console.log(options);
-    var req = http.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    });
-    req.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-    });
-    // write data to request body
-    req.write('data\n');
-    req.write('data\n');
-    req.end();
-}
-
-// Letters can/will be added eventually.
-
-var States = ['Home', 'Rev', 'Fwd', 'Play', 'Select', 'Left', 'Right', 'Down', 'Up', 'Back', 'InstantReplay', 'Info', 'Backspace', 'Search', 'Enter'  ]
-
-/*
- * Firebase State Manager
- */
+//http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4
+   // Put logic here for state changes
 myRootRef.on('child_changed', function(childSnapshot, prevChildName) {
-    // code to handle child data changes.
-    var data = childSnapshot.val();
-    var localref = childSnapshot.ref();
-    // Send command to roku below
-    if (States.indexOf(data["state"]) >= 0) {
-        console.log('Change State to ' + data["state"]);
-        send_message({
-            hostname: data["ip"],
-            port: 8060,
-            path: '/keypress/' + data["state"],
-            method: 'POST'
-        });
-        localref.update({
-            "state": "waiting"
-        });
-        console.log("Back to Waiting...");
-    }
-    
-
-});
-console.log("setup on callback");
-
-
-
-
-/*
- * Shutting down stuff
- */
-process.stdin.resume(); //so the program will not close instantly
-function delete_fb_entries() {
-    return function() {
-        for (i = 0; i < devices.length; i++) {
-            devices[i].remove();
-        }
-        process.exit()
-    }
+// code to handle child data changes.
+var data = childSnapshot.val();
+var localref = childSnapshot.ref();
+if (data["state"] == "play")
+{
+device.play(data["movie"], 60, function(){
+        console.log('Playing in your chromecast!')
+    });
 }
-
-//do something when app is closing
-process.on('exit', delete_fb_entries());
-
-//catches ctrl+c event
-process.on('SIGINT', delete_fb_entries());
-
-//catches uncaught exceptions
-process.on('uncaughtException', delete_fb_entries());
+})
+  })
+})
